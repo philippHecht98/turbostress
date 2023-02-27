0457b4c46f673edc612153c7ce48a63a5dec1bb5
Philipp Hecht (PT-301) <Philipp.Hecht@bmw.de>
2023-02-27 10:40:54 +0100

    dev

--- cmd/ram_usage.py
+++ cmd/ram_usage.py
@@ -10,8 +10,9 @@ for filename in os.listdir():
             while index < len(lines):
                 line = str(lines[index])
                 data = line.split()
+                print(data)
                 total = data[1]
                 free = data[3]
-                print("test: {} free: {} total: {} ratio: {}".format(testcase, free, total, free / total))
+                print("test: {} free: {} total: {} ratio: {}".format(testcase, free, total, int(free) / int(total)))
                 index += 3
                 
